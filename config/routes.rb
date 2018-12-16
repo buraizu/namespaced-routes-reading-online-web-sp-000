873c57552431f3dd1557118c4d4bb4bfaa6e353f
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-16 20:27:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
   get '/admin/authors/create', to: 'authors#create'
   get '/admin/comments/moderate', to: 'comments#moderate'
 
+  scope '/admin' do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end
